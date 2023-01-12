a29e72d7b9d6c6d5516b81900ae9a649ddd40959
norkiii <norlanviga12@gmail.com>
2023-01-12 17:06:39 +0800

    Updates

---
+++ precache-manifest.9c512021414b3694f6128eebbca1de61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "44fdef9bd89d86e5a8e7",
+    "url": "/my-site/static/css/main.53e05ba1.chunk.css"
+  },
+  {
+    "revision": "44fdef9bd89d86e5a8e7",
+    "url": "/my-site/static/js/main.44fdef9b.chunk.js"
+  },
+  {
+    "revision": "cc519580b8acf670d6e7",
+    "url": "/my-site/static/js/1.cc519580.chunk.js"
+  },
+  {
+    "revision": "5c22e95fac8b423985b8",
+    "url": "/my-site/static/js/runtime~main.5c22e95f.js"
+  },
+  {
+    "revision": "70ce64e7656e2d2bafe3d6fff1d473a2",
+    "url": "/my-site/index.html"
+  }
+];
